8891e6fc7b15f1953247841450b6878233a3597e
gena-rybin <genarybin1979@gmail.com>
2016-10-31 19:38:55 +0200

    all elements are created dynamically;  on game's field mouseover  active ship is displayed

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ window.onload  = function () {
     var data = [];
     var x=0;
     var y=0;
+    var shipLength = 0;
 
 
     var yy=0;
@@ -63,6 +64,9 @@ window.onload  = function () {
 
     var divShip = div.cloneNode(false);
     divShip.className = 'ship';
+
+    var divShipCell = div.cloneNode(false);
+    divShipCell.classList.add("shipCell");
 
 
 // page loading
@@ -114,9 +118,8 @@ window.onload  = function () {
             for (var j=1; j<=i; j++) {
                 divShip = divShip.cloneNode(false);
                     for (var cell=5-i; cell>=1; cell--) {
-                        divGameCell = divGameCell.cloneNode(false);
-                        divGameCell.classList.remove('free');
-                        divShip.appendChild(divGameCell);
+                        divShipCell = divShipCell.cloneNode(false);
+                        divShip.appendChild(divShipCell);
                     }
                 divShips.appendChild(divShip);
             }
@@ -130,29 +133,83 @@ window.onload  = function () {
     function getPosition() {
         event.target.parentNode.childNodes.forEach(function(item, i) {
             if (item === event.target) {
-                x = Math.floor(i/2);
+                x = i;
             }
         });
         event.target.parentNode.parentNode.childNodes.forEach(function(item, index) {
             if (item === event.target.parentNode) {
-                y = Math.floor(index/2);
+                y = index;
             }
         });
-        console.log('hovered: строка='+y+', ячейка='+x);
+        //console.log('hovered: строка='+y+', ячейка='+x);
     }
 
+    var horiz = false;
+    var vert = true;
 
     divCover.onmouseover = function(event) {
         getPosition();
         var cell = event.target;
-        event.target.classList.add("hover");
-        //gameCell[5].classList.add("hover");
+        //event.target.classList.add("hover");
+        if (horiz==true) {
+            for (var i=0; i<shipLength; i++) {
+                if (divGameRows.childNodes[y].childNodes[x+shipLength-1]) {
+                    divGameRows.childNodes[y].childNodes[x+i].classList.add("hover");
+                }
+            }  //if(data[y-1] && data[y-1][x-1] === 1)
+        }
+        if (vert==true) {
+            for (var i = 0; i < shipLength; i++) {
+                if (divGameRows.childNodes[y + shipLength -1]) {
+                    divGameRows.childNodes[y + i].childNodes[x].classList.add("hover");
+                }
+            }
+        }
+        //gameCell[15].classList.add("hover");
         //console.log('hovered: строка='+y+', ячейка='+x);
     }
     divCover.onmouseout = function(event) {
         var cell = event.target;
         cell.classList.remove("hover");
-        gameCell[5].classList.remove("hover");
+        if (horiz==true) {
+            for (var i=1; i<shipLength; i++) {
+                divGameRows.childNodes[y].childNodes[x+i].classList.remove("hover");
+            }
+        }
+        if (vert==true) {
+            for (var i=1; i<shipLength; i++) {
+                //gameCell[10*y+x+10*i].classList.remove("hover");
+                divGameRows.childNodes[y+i].childNodes[x].classList.remove("hover");
+            }
+        }
     }
+
+    // choose ship to set on the game's field
+    divControl.addEventListener("click", arrangeShips);
+    function arrangeShips(event) {
+        removeClassFromNeighbors('checked');
+        var shipActive = event.target.parentNode;
+        if (shipActive.className == 'ship') {    // ship checking
+            shipLength = shipActive.childNodes.length;
+            shipActive.classList.add('checked');
+            // if (shipActive.classList.contains('vertical')) {
+            //     shipActive.classList.remove('vertical');
+            // }
+            // shipActive.classList.add('vertical');
+                //.vertical
+        }
+
+    }
+
+    function removeClassFromNeighbors (name) {
+        for (var i = 0; i<divHarbor.childNodes.length; i++) {
+            for (var j = 0; j<divHarbor.childNodes[i].childNodes.length; j++) {
+                if (divHarbor.childNodes[i].childNodes[j].classList.contains(name)) {
+                    divHarbor.childNodes[i].childNodes[j].classList.remove(name);
+                }
+            }
+        }
+    }
+
 
 }
